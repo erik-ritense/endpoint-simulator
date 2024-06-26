fa4fcbfad76b8643bc4cb85e4b62190e5432b357
Erik de Vries <erik.de.vries@ritense.com>
2024-06-26 22:22:14 +0200

    added slow endpoint that returns data

--- src/main/kotlin/com/ritense/apierik/web/ApiController.kt
+++ src/main/kotlin/com/ritense/apierik/web/ApiController.kt
@@ -1,13 +1,17 @@
 package com.ritense.apierik.web
 
+import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody
+import java.io.IOException
 
 @RestController
 class ApiController {
@@ -19,6 +23,15 @@ class ApiController {
         Thread.sleep(100000000)
         return ApiResponse("hello", "world")
     }
+
+    // Simulates a Openzaak endpoint
+    @GetMapping("/zaken/api/v1/zaken/{uuid}")
+    fun getZakenBla(@PathVariable uuid: String): ApiResponse {
+        logger.info("Got a GET request")
+        Thread.sleep(100000000)
+        return ApiResponse("hello", "world")
+    }
+
 
     // Simulates Smart Documents endpoint
     @PostMapping("/wsxmldeposit/deposit/unattended")
@@ -46,5 +59,25 @@ class ApiController {
     companion object{
         private val logger = LoggerFactory.getLogger(this::class.java)
 
+    }
+
+    @GetMapping("/slow-response")
+    fun slowResponse(response: HttpServletResponse): StreamingResponseBody {
+        return StreamingResponseBody { outputStream ->
+            outputStream.bufferedWriter().use { writer ->
+                for (i in 1..60) {
+                    writer.write("Chunk $i of data\n")
+                    writer.flush()
+                    try {
+                        Thread.sleep(1000) // sleep for 1 second
+                    } catch (e: InterruptedException) {
+                        Thread.currentThread().interrupt()
+                        throw IOException("Thread interrupted", e)
+                    }
+                }
+                writer.write("Done\n")
+                writer.flush()
+            }
+        }
     }
 }
